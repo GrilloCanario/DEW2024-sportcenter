8d44fbf6505bf028cf32e495a612e7ea2020d6f8
PedroBarrera <pedrodbarrerafdez@gmail.com>
2024-10-15 03:21:22 +0100

    1º contacto

---
+++ src/sport-center.js
@@ -0,0 +1,10 @@
+class SportCenter {
+    #services
+  constructor (name, fee, membership) {
+    this.name = name
+    this.fee = fee
+    this.membership = membership
+  }
+}
+
+module.exports = SportCenter
