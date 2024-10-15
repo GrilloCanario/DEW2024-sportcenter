d6507ea874690528d9f8f78a909f87fbe39fd488
PedroBarrera <pedrodbarrerafdez@gmail.com>
2024-10-15 03:31:35 +0100

    1c

---
+++ src/service.js
@@ -0,0 +1,8 @@
+class service {
+  constructor (name, ratings) {
+    this.name = name
+    this.ratings = ratings
+  }
+}
+
+module.exports = service
--- src/sport-center.js
+++ src/sport-center.js
@@ -1,9 +1,13 @@
 class SportCenter {
-    #services
-  constructor (name, fee, membership) {
+  #services
+  constructor (name, fee = 0, membership = 0) {
     this.name = name
     this.fee = fee
     this.membership = membership
+  }
+
+  income () {
+    return (this.fee * this.membership)
   }
 }
 
