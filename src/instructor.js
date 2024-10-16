77a71dca25c39e44af2ee4e00a0727afb2eb8d25
PedroBarrera <pedrodbarrerafdez@gmail.com>
2024-10-16 01:36:05 +0100

    test 5 completed

--- src/instructor.js
+++ src/instructor.js
@@ -1,11 +1,25 @@
-class instructor {
+const Activity = require('./activity')
+
+class Instructor {
   #basicSalary
   constructor (name, basicSalary = 500) {
     this.name = name
+    this.ledActivities = []
     this.#basicSalary = basicSalary
   }
 
   get salary () {
-    
+    let total = this.#basicSalary
+    this.ledActivities.forEach(element => {
+      total += element.assitance * 5 + 100
+    })
+    return total
   }
+  /* OPCION PROFESOR
+  get salary () {
+    return this.#basicSalary +
+        this.ledActivities.reduce((t, a) => t + 100 + 5 * a.assistance, 0)
+  }
+  */
 }
+module.exports = Instructor
